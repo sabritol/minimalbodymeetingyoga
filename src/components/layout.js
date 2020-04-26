d22cb6d63ceb417451523497d0e50477a785e4bd
sabrina <sabrinatolmer@gmail.com>
2020-04-26 19:02:07 +0200

    fixed icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,11 @@ import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import Navigation from "../components/navigation"
 import 'prismjs/themes/prism-okaidia.css';
+import logo from './../../public/assets/logo.svg'
+import facebook from './../../public/assets/Facebook.png'
+import instagram from './../../public/assets/Instagram.png'
+import twitter from './../../public/assets/Twitter.png'
+import youtube from './../../public/assets/Youtube.png'
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -25,7 +30,54 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; 2020 Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
+      <div className="content has-text-centered">
+          <img
+            src={logo}
+            alt="Kaldi"
+            style={{ width: '14em', height: '10em' }}
+          />
+        </div>
+        <div className="content has-text-centered has-background-black has-text-white-ter">
+          <div className="container has-background-black has-text-white-ter">
+            <div className="columns">
+              <div className="column is-4">
+              </div>
+              <div className="column is-4">
+              </div>
+              <div className="social-icons">
+                <a title="facebook" href="https://www.facebook.com/CaroYoga-183518078972158/">
+                  <img
+                    src={facebook}
+                    alt="Facebook"
+                    style={{ width: '2.5em', height: '2.5em' }}
+                  />
+                </a>
+                <a title="twitter" href="https://twitter.com/CaroSambado">
+                  <img
+                    className="fas fa-lg"
+                    src={twitter}
+                    alt="Twitter"
+                    style={{ width: '2.5em', height: '2.5em' }}
+                  />
+                </a>
+                <a title="instagram" href="https://www.instagram.com/carosolyogapilates/?hl=es">
+                  <img
+                    src={instagram}
+                    alt="Instagram"
+                    style={{ width: '2.5em', height: '2.5em' }}
+                  />
+                </a>
+                <a title="youtube" href="https://vimeo.com">
+                  <img
+                    src={youtube}
+                    alt="youtube"
+                    style={{ width: '2.5em', height: '2.5em' }}
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </footer>
     </div>
   )
