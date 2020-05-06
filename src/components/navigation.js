7a23fa7d0b9224927460e5a5c7570eb7fd686e1d
sabrina <sabrinatolmer@gmail.com>
2020-05-06 21:25:57 +0200

    update config.yml

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
+    <Link to="/contact">CONTACT</Link>
     <ThemeChanger/>
   </nav>
   
